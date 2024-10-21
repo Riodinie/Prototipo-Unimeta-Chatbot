1c9ed01d9056b26e207f7efd2e87725e9543500d
YokoMari <mariagarciariver223@gmail.com>
2024-10-21 14:26:12 -0500

    Actualizar Comentarios
    Se actualizo los comentarios que explican el funcionamiento del codigo <3

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+# Librerias
+from flask import Flask, render_template, request, jsonify # Crear Apps web
+from chat import get_response                              # Procesa Chats (mensajes)
 
-from chat import get_response
-
+# Inicializacion de la app Flask
 app = Flask(__name__)
 
+# Ruta que responde a Get
 @app.get("/")
 def index_get():
-    return render_template ("index.html")
+    return render_template ("index.html")   # Renderiza el HTML
 
-
+# Ruta que responde a Post (Usuario envia el mensaje y se genera una respuesta)
 @app.post("/predict")
+# Metodo de respuesta del Chatbot
 def predict():
-    text = request.get_json().get("message")
-    # TODO: check if text is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    text = request.get_json().get("message")    # Obtiene la pregunta
+    response = get_response(text)               # Funcion con el mensaje del Usuario para obtener una respuesta
+    message = {"answer": response}              # Diccionario con la respuesta
+    return jsonify(message)                     # Diccionario a JSON y envia la respuesta al usuario
 
+# Ejecutar App
 if __name__ == "__main__":
     app.run(debug=True)
