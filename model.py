9704361e0afbb29bc1254882eb249b4ae78662e6
YokoMari <mariagarciariver223@gmail.com>
2024-10-21 14:03:05 -0500

    Actualizar Comentarios
    Se agrego y edito los comentarios que explican el codigo <3

--- model.py
+++ model.py
@@ -1,20 +1,29 @@
-import torch
-import torch.nn as nn
+# Librerias 
+import torch #Deep Learning
+import torch.nn as nn #Modulo RNA
 
-
+# RNA simple (3) Capas
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.l2 = nn.Linear(hidden_size, hidden_size) 
-        self.l3 = nn.Linear(hidden_size, num_classes)
+
+        # Capas = (Input-#strEntrada) (Hidden-#N¿neuronasCOculta) (num-#clasesSalida)
+        self.l1 = nn.Linear(input_size, hidden_size)  # Entrada -> Oculta
+        self.l2 = nn.Linear(hidden_size, hidden_size) # Oculta -> Oculta
+        self.l3 = nn.Linear(hidden_size, num_classes) # Oculta -> Salida
+
+        # Funcion de Activacion
         self.relu = nn.ReLU()
     
+    # Metodo Flujo de Datos de la RNA
     def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        out = self.relu(out)
-        out = self.l3(out)
-        # no activation and no softmax at the end
+
+        # Procesamiento de la RNA
+        out = self.l1(x)        # Entra 1ra Capa O.
+        out = self.relu(out)    # Aplica la Func.Activ.
+        out = self.l2(out)      # Entra 2da Capa O.
+        out = self.relu(out)    # Aplica la Func.Activ.
+        out = self.l3(out)      # Sale Capa Salida
+
+        # No se aplica ninguna función de activación en la salida final
         return out
