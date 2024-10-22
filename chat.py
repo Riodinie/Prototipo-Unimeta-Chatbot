2ad31383b8a378546cdb70680c52c7eb8a948387
YokoMari <mariagarciariver223@gmail.com>
2024-10-21 22:27:27 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -11,7 +11,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Archivo del Entrenamiento / Intenciones del Chatbot
 with open('intents.json', 'r', encoding='utf-8') as json_data:
-    intents = json.load(json_data)  # JSON en un diccionario (Intenciones y Respuestas)
+    intents = json.load(json_data)              # JSON en un diccionario (Intenciones y Respuestas)
 
 # Datos despues del entrenamiento
 FILE = "data.pth"
@@ -19,42 +19,42 @@ data = torch.load(FILE)
 
 # Extraer Informacion
 # Dimensiones de la Red Cargada
-input_size = data["input_size"]     # #str de Entrada
-hidden_size = data["hidden_size"]   # Tamaño Capa Oculta (RNA)
-output_size = data["output_size"]   # #Intenciones (Clases)
+input_size = data["input_size"]                 # #str de Entrada
+hidden_size = data["hidden_size"]               # Tamaño Capa Oculta (RNA)
+output_size = data["output_size"]               # #Intenciones (Clases)
 
-all_words = data['all_words']       # Lista Palabras unicas
-tags = data['tags']                 # Lista Posibles Etiquetas Intenciones
-model_state = data["model_state"]   # Estado del Modelo (pesos)
+all_words = data['all_words']                   # Lista Palabras unicas
+tags = data['tags']                             # Lista Posibles Etiquetas Intenciones
+model_state = data["model_state"]               # Estado del Modelo (pesos)
 
 #Inicializacion del Modelo RNA (ya entrenado)
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)  # Carga los Pesos (entrenamiento)
-model.eval()                        # Solo predicciones (sin entrenamiento)
+model.load_state_dict(model_state)              # Carga los Pesos (entrenamiento)
+model.eval()                                    # Solo predicciones (sin entrenamiento)
 
 # Nombre del Chatbot
 bot_name = "Tyto"
 
 # Funcion que procesa el Mensaje del Usuario
 def get_response(msg):
-    sentence = tokenize(msg)                # Mensaje a lista
-    X = bag_of_words(sentence, all_words)   # Palabras a vectores
-    X = X.reshape(1, X.shape[0])            # Ajusta el vector
-    X = torch.from_numpy(X).to(device)      # Vector a Tensor (@)
+    sentence = tokenize(msg)                    # Mensaje a lista
+    X = bag_of_words(sentence, all_words)       # Palabras a vectores
+    X = X.reshape(1, X.shape[0])                # Ajusta el vector
+    X = torch.from_numpy(X).to(device)          # Vector a Tensor (@)
 
     # Proceso del modelo
-    output = model(X)                       # Entrada -> Modelo -> Salida
-    _, predicted = torch.max(output, dim=1) # Encuentra Intencion (clase) con mas valor en salida
+    output = model(X)                           # Entrada -> Modelo -> Salida
+    _, predicted = torch.max(output, dim=1)     # Encuentra Intencion (clase) con mas valor en salida
 
     # Etiqueta de la Intencion Predicha
-    tag = tags[predicted.item()]            # Indice de la Intencion (clase) predicha
+    tag = tags[predicted.item()]                # Indice de la Intencion (clase) predicha
 
     # Calcular Probabilidades
-    probs = torch.softmax(output, dim=1)    # Salida a % (0 y 1)
-    prob = probs[0][predicted.item()]       # % de la Intencion Predicha
+    probs = torch.softmax(output, dim=1)        # Salida a % (0 y 1)
+    prob = probs[0][predicted.item()]           # % de la Intencion Predicha
 
     # Comparar % con un Umbral
-    if prob.item() > 0.75:                  # Si la Intencion Predicha es mayor de 75% es confiable
+    if prob.item() > 0.75:                      # Si la Intencion Predicha es mayor de 75% es confiable
         
         # Responder a la Intencion
         for intent in intents['intents']:                   # Lista Intenciones
